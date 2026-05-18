ba922cc900b95a88ff300a99e353128370240263
MatseVH <49431416+Matse2005@users.noreply.github.com>
2026-05-18 14:43:09 +0200

    Added a second radio component to test

--- components/widgets/index.ts
+++ components/widgets/index.ts
@@ -1,15 +1,38 @@
+import { Widget } from "@/types";
 import PhoneWidget from "./phone";
 import RadioWidget from "./radio";
+import { PhoneIcon, RadioIcon } from "lucide-react";
+import Radio2Widget from "./radio2";
 
-const widgets = [
-  {
-    id: "radio",
-    widget: RadioWidget
-  },
-  {
-    id: "phone",
-    widget: PhoneWidget
-  },
-];
+const getAll = (): Widget[] => {
+  return [
+    {
+      id: "radio",
+      name: "Radio",
+      widget: RadioWidget,
+      icon: RadioIcon
+    },
+    {
+      id: "radio2",
+      name: "Radio2",
+      widget: Radio2Widget,
+      icon: RadioIcon
+    },
+    {
+      id: "phone",
+      name: "Phone",
+      widget: PhoneWidget,
+      icon: PhoneIcon
+    },
+  ];
+}
 
-export default widgets;
+const getById = (id: string): number => {
+  const widgets = getAll();
+  const index = widgets.findIndex(widget => widget.id === id);
+  return index !== -1 ? index : 0;
+}
+
+const exportable = { getAll, getById }
+
+export default exportable;
---
+++ components/widgets/radio2.tsx
@@ -0,0 +1,87 @@
+"use client";
+import channels from "@/lib/radio/index2";
+import { useRef, useState } from "react";
+import Widget from "../widget";
+import Status from "../status";
+import { Button } from "../ui/button";
+
+export default function Radio2Widget() {
+  const [radioIndex, setRadioIndex] = useState<number>(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isBuffering, setIsBuffering] = useState(false);
+
+  const channel = channels[radioIndex];
+
+  return (
+    <Widget
+      title={channel ? `Radio2 - ${channel.name}` : "Radio"}
+      image={
+        channel && channel.imageUrl !== null
+          ? channel.imageUrl
+          : "/images/radio.png"
+      }
+      loading={false}
+      imageLoading={false}
+    >
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-3">
+          {isBuffering && (
+            <Status
+              description="Starting radio..."
+              Icon={"loading"}
+              variant="neutral"
+            />
+          )}
+          {channel && (
+            <div className="flex flex-col items-center gap-3">
+              <audio
+                ref={audioRef}
+                controls
+                className="hidden"
+                autoPlay
+                src={channel.streamUrl}
+                onWaiting={() => setIsBuffering(true)}
+                onCanPlay={() => setIsBuffering(false)}
+                onPlay={() => {
+                  if (audioRef.current) {
+                    const audio = audioRef.current;
+                    if (audio.buffered.length > 0) {
+                      audio.currentTime = audio.buffered.end(
+                        audio.buffered.length - 1,
+                      );
+                    }
+                    setIsBuffering(false);
+                  }
+                }}
+              />
+              <div className="flex gap-2">
+                <Button
+                  size="lg"
+                  onClick={() => {
+                    setRadioIndex((i) =>
+                      i === 0 ? channels.length - 1 : i - 1,
+                    );
+                    setIsBuffering(true);
+                  }}
+                >
+                  ← Prev
+                </Button>
+                <Button
+                  size="lg"
+                  onClick={() => {
+                    setRadioIndex((i) =>
+                      i === channels.length - 1 ? 0 : i + 1,
+                    );
+                    setIsBuffering(true);
+                  }}
+                >
+                  Next →
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </Widget>
+  );
+}
